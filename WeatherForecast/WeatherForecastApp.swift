6b70e9eed3e08ada01f5763e17cdcb472973e257
Sophie Haber <SHaber@lcs.on.ca>
2024-01-14 09:16:45 -0500

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Sophie Haber on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
