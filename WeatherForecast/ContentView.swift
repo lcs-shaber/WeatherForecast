370633562ab251674384980ced245ba72f204881
Sophie Haber <SHaber@lcs.on.ca>
2024-01-14 10:00:41 -0500

    I finished the weather icons.

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -12,10 +12,10 @@ struct ContentView: View {
         HStack {
             
             
-            DayForecast(day: "Mon", high: 70, low: 50)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
             
             
-            DayForecast(day: "Tue", high: 60, low: 40)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
             
                 
         }
@@ -28,17 +28,40 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
+    
+    var iconName: String {
+        if isRainy{
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
     
     var body: some View {
         VStack {
             
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
             Text("High: \(high)º F")
+                .fontWeight(Font.Weight.semibold)
             Text("Low: \(low)º F")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
             
         }
         .padding()
